ee2dac0f0cb608979c61ae4ab4adb90a52db1c75
KAI <KAI@121.128.64.239>
2020-10-20 00:18:11 +0900

    1차진행분

---
+++ algo-study/src/S201019_BJ11047.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class S201019_BJ11047 {
+
+	static int N ,M;
+	static int[] a;
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+
+	}
+
+}
